6ea75bb83007a9040ed5ca37d159430088a35716
Art <developer@Art-LUX.local>
2022-12-01 17:20:44 +0300

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MDFInternationalization",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "MDFInternationalization",
+            targets: ["MDFInternationalization"]),
+    ],
+    targets: [
+        .target(
+            name: "MDFInternationalization",
+            path: "Sources"
+        ),
+    ]
+)
